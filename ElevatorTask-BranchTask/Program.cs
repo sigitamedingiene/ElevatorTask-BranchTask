c8e558dcec02b60fca225ad6cf68e99987d98ca1
sigitamedingiene <sigitamedingiene@gmail.com>
2022-05-12 18:05:19 +0300

    Add Service class. Created Elevator moving method.

--- ElevatorTask-BranchTask/Program.cs
+++ ElevatorTask-BranchTask/Program.cs
@@ -12,19 +12,14 @@ namespace ElevatorTask_BranchTask
             BuildingRepository buldingList = new BuildingRepository();
             ElevatorRepository elevatorList = new ElevatorRepository();
 
+
             buldingList.GetBuildingData();
             Building building = new Building(buldingList.BuildingAdress, buldingList.FloorCount, buldingList.ElevatorStandsInWichFloor, buldingList.ElevatorId);
             elevatorList.GetElevatorData();
             Elevator elevator = new Elevator(elevatorList.ElevatorId, elevatorList.MaxCapacity, elevatorList.ElevatorStayingFloor);
 
-            Console.WriteLine("Write how many people in kg want to use the elevator.");
-            int peopleCapacityInKg = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write in wich floor you want to go.");
-            int floorNumber = Convert.ToInt32(Console.ReadLine());
-
-            elevator.ElevatorMoves(peopleCapacityInKg, floorNumber);
-            Console.WriteLine("Elevator stops in:");
-            elevator.FloorElevatorStops(floorNumber);
+            ElevatorMovment elevatorMovement = new ElevatorMovment(elevator);
+            elevatorMovement.ElevatorMoving();
         }
     }
 }
