a29c1666ae9202323d07bfd71f50b7179d9e50d1
sigitamedingiene <sigitamedingiene@gmail.com>
2022-05-12 15:24:19 +0300

    Add project files.

---
+++ ElevatorTask-BranchTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElevatorTask_BranchTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
