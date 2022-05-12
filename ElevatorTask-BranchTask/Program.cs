987161a50c3209fd89daa1990fbf351eec211dc4
sigitamedingiene <sigitamedingiene@gmail.com>
2022-05-12 15:39:21 +0300

    Project copied to branch

--- ElevatorTask-BranchTask/Program.cs
+++ ElevatorTask-BranchTask/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BussnesClass;
+using Repository;
+using System;
 
 namespace ElevatorTask_BranchTask
 {
@@ -7,6 +9,22 @@ namespace ElevatorTask_BranchTask
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BuildingRepository buldingList = new BuildingRepository();
+            ElevatorRepository elevatorList = new ElevatorRepository();
+
+            buldingList.GetBuildingData();
+            Building building = new Building(buldingList.BuildingAdress, buldingList.FloorCount, buldingList.ElevatorStandsInWichFloor, buldingList.ElevatorId);
+            elevatorList.GetElevatorData();
+            Elevator elevator = new Elevator(elevatorList.ElevatorId, elevatorList.MaxCapacity, elevatorList.ElevatorStayingFloor);
+
+            Console.WriteLine("Write how many people in kg want to use the elevator.");
+            int peopleCapacityInKg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write in wich floor you want to go.");
+            int floorNumber = Convert.ToInt32(Console.ReadLine());
+
+            elevator.ElevatorMoves(peopleCapacityInKg, floorNumber);
+            Console.WriteLine("Elevator stops in:");
+            elevator.FloorElevatorStops(floorNumber);
         }
     }
 }
